41f4fb95a2b83aa8e86e8f438d85c3ef98cc5aa9
Titov Dmitry <dm.a.titov@ya.ru>
2024-02-29 14:13:34 +0300

    refactor: remove .idea

--- src/bin/clack.rs
+++ src/bin/clack.rs
@@ -1,13 +1,11 @@
 fn main() {
-    let config = clack::Config::cli()
-        .unwrap_or_else(|err| {
-            eprintln!("Problem parsing arguments: {err}");
-            std::process::exit(1);
-        });
-    let result = clack::calc(config).
-        unwrap_or_else(|err| {
-            eprintln!("Failed to evaluate query: {err}");
-            std::process::exit(0);
-        });
+    let config = clack::Config::cli().unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
+        std::process::exit(1);
+    });
+    let result = clack::calc(config).unwrap_or_else(|err| {
+        eprintln!("Failed to evaluate query: {err}");
+        std::process::exit(0);
+    });
     println!("{result:?}")
 }
